fc79d66664b0d0e52076a5cee2f0cf0a6e68e465
Aneeq Ullah Khan <aneequllah.khan@outlook.com>
2022-12-15 02:21:39 +0500

    laravel project

---
+++ resources/views/user/main.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Users</title>
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+</head>
+<body>
+  
+<div class="container">
+    @yield('content')
+</div>
+   
+</body>
+</html>
